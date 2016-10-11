56893fcb6db2328b7067dacb66743f4132bfdecc
secilusta <secil095@gmail.com>
2016-10-11 20:56:41 +0300

    example code is changed to my own code

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,7 @@
-def search(array, element):
+def search(array, element1, element2):
   for e in array:
-    if e == element:
-      return True
+    if e == element1:
+      for f in array:
+        if f == element2:
+          return True
   return False
--- test_algorithm.py
+++ test_algorithm.py
@@ -6,22 +6,31 @@ class TestSearch(unittest.TestCase):
     self.array = [10, 5, 20, 30, 1, 4]
 
   def test_successful(self):
-    self.assertTrue(search(self.array, 20))
+    self.assertTrue(search(self.array, 20, 30))
+
+  def test_successful2(self):
+    self.assertTrue(search(self.array, 1, 5))
 
   def test_failed(self):
-    self.assertFalse(search(self.array, 100))
+    self.assertFalse(search(self.array, 5, 12))
+
+  def test_failed2(self):
+    self.assertFalse(search(self.array, 13, 1))
+
+  def test_failed3(self):
+    self.assertFalse(search(self.array, 13, 14))
 
   def test_emptyArray(self):
-    self.assertFalse(search([], 10))
+    self.assertFalse(search([], 10 , 1))
 
   def test_whenArrayIsNone(self):
-    self.assertFalse(None, 10)
+    self.assertFalse(search(None, 10, 11))
 
   def test_searchingNoneFailed(self):
-    self.assertFalse(search(self.array, None))
+    self.assertFalse(search(self.array, None, None))
 
   def test_searchingNoneSuccessful(self):
-    self.assertTrue(search([None], None))
+    self.assertTrue(search([None], None, None))
 
 
 if __name__ == '__main__':
