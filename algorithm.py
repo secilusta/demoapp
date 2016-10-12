601b2a6fbe00e6b63486dfcf5eaf95affd8fabfe
secilusta <secil095@gmail.com>
2016-10-13 01:45:42 +0300

    Tiny change for coverage

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,10 @@
 def search(array, element1, element2):
   if(array == None):
     return False
-  for e in array:
-    if str(e) == str(element1).strip(' '):
-      for f in array:
-        if str(f) == str(element2).strip(' '):
-          return True
+  else:
+    for e in array:
+      if str(e) == str(element1).strip(' '):
+        for f in array:
+          if str(f) == str(element2).strip(' '):
+            return True
   return False
